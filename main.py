e90a88a1893427e5b502f2745953817e12efc993
Sebastian Granda Gallego <sebastian.granda@jikkosoft.com>
2021-08-18 19:10:50 -0500

    Finished scraper

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
 import argparse
+
+from requests.models import HTTPError
+from urllib3.exceptions import MaxRetryError
 from common import config
 import logging
+import re
+
+import news_page_objects as news
 
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+IS_WELL_FOR_LINK = re.compile(r'^https?://.+/.+$')
+IS_ROOT_PATH = re.compile(r'^/.+$')
+
+
 def _news_scraper(news_site_uid):
     host = config()["news_sites"][news_site_uid]["url"]
 
-    logging.info(f"Beginning scraper for {host}")
+    logger.info(f"Beginning scraper for {host}")
+
+    homepage = news.HomePage(news_site_uid, host)
+
+    articles = []
+    for link in homepage.article_links:
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info("Article fetched!!")
+            articles.append(article)
+            print(article.title)
+    print(len(articles))
+
+def _build_link(host, link):
+    if IS_WELL_FOR_LINK.match(link):
+        return link
+    elif IS_ROOT_PATH.match(link):
+        return f"{host}{link}"
+    return f"{host}/{link}"
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f"Start fetching article at {link}")
+
+    article = None
+
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning(f"Error while fetching the article", exc_info=False)
+
+    if article and not article.body:
+        logger.warning("Invalid article. There is no body")
+        return None
+
+    return article
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
---
+++ news_page_objects.py
@@ -0,0 +1,54 @@
+import requests
+import lxml.html as html
+from common import config
+
+class NewsPage:
+
+    def __init__(self, news_site_uid: str, url: str) -> None:
+        self.__config = config()["news_sites"][news_site_uid]
+        self._queries = self.__config["queries"]
+        self._html = None
+
+        self._visit(url)
+
+    def _select(self, query_string):
+        return self._html.xpath(query_string)
+
+    def _visit(self, url: str) -> None:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        page = response.content.decode("utf-8")
+        self._html = html.fromstring(page)
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid: str, url: str) -> None:
+        super().__init__(news_site_uid, url)
+
+    @property
+    def article_links(self):
+        return { link for link in self._select(self._queries["homepage_article_links"]) if link }
+
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid: str, url: str) -> None:
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        try:
+            print(self._queries["article_body"])
+            result = self._select(self._queries["article_body"])
+            return result[0].replace('"', '') if len(result) else ""
+        except IndexError:
+            return ""
+
+    @property
+    def title(self):
+        try:
+            result = self._select(self._queries["article_title"])
+            return result[0].replace('"', '') if len(result) else ""
+        except IndexError:
+            return ""
+
